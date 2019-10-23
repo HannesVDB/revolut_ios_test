a44cc6bd97568db45fa7b03c39d0e8769cd19308
Hannes Van den Berghe <hannes.vandenberghe@icapps.com>
2019-10-23 17:00:15 +0200

    Initial commit

---
+++ revolut_ios_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  revolut_ios_test
+//
+//  Created by Hannes Van den Berghe on 23/10/2019.
+//  Copyright © 2019 HannesVDB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
